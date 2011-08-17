1c717a2b14e3e545922f5aea62a128f6c401b31b
Kennric <kennric@hypothetical.net>
2011-08-17 13:40:44 -0700

    rename files as well as retag

--- filecleaner.py
+++ filecleaner.py
@@ -5,25 +5,25 @@ import re
 import time
 import mutagen
 from mutagen.easyid3 import EasyID3
-    
+from mutagen.mp3 import MP3
+from mutagen.id3 import ID3
+
 #audio = EasyID3("example.mp3")
 #audio["title"] = u"An example"
 #audio.save()
 
-from mutagen.easyid3 import EasyID3
-print EasyID3.valid_keys.keys()
 
 config = ConfigParser.ConfigParser()
 config.read('podcast.cfg')
 
 tmpdir = config.get('files', 'tmpdir')
 storage = config.get('files', 'storage')
-podcast_name = config.get('info', 'shortname')
+podcast_name = config.get('info', 'name')
+podcast_shortname = config.get('info', 'shortname')
+podcast_station = config.get('info', 'station')
 
-# available tags: ['albumartistsort', 'musicbrainz_albumstatus', 'lyricist', 'releasecountry', 'date', 'performer', 'musicbrainz_albumartistid', 'composer', 'encodedby', 'tracknumber', 'musicbrainz_albumid', 'album', 'asin', 'musicbrainz_artistid', 'mood', 'copyright', 'author', 'media', 'length', 'version', 'artistsort', 'titlesort', 'discsubtitle', 'website', 'musicip_fingerprint', 'conductor', 'compilation', 'barcode', 'performer:*', 'composersort', 'musicbrainz_discid', 'musicbrainz_albumtype', 'genre', 'isrc', 'discnumber', 'musicbrainz_trmid', 'replaygain_*_gain', 'musicip_puid', 'artist', 'title', 'bpm', 'musicbrainz_trackid', 'arranger', 'albumsort', 'replaygain_*_peak', 'organization']
 
 files = os.listdir(tmpdir)
-
 
 old_date = ""
 file_dict = {}
@@ -32,11 +32,16 @@ for file in files:
   tmpath = tmpdir + '/' + file
 
   date = re.search('[0-9]{0,2}-[0-9]{0,2}-[0-9]{4}(?!\d)',file).group()
+  date_pieces = date.split("-")
+  year = date_pieces[2]
+  month = date_pieces[0]
+  day = date_pieces[1]
+
   tm = re.search('[0-9]{0,2}-[0-9]{2}-[0-9]{2}\s[\w]{2}(?!\d)',file).group()
 
   tm_pieces = tm.split('-')
-  hour = int(tm_pieces[0])
-  minute = int(tm_pieces[1])
+  hour = tm_pieces[0]
+  minute = tm_pieces[1]
 
   second_pieces = tm_pieces[2].split()
   second = int(second_pieces[0])
@@ -46,20 +51,31 @@ for file in files:
     if hour < 12:
       hour += 12
 
-  good_time = time.strptime(date + "-" + `hour` + "-" + `minute` + "-" + `second`, "%m-%d-%Y-%H-%M-%S")
-
-  print int(time.mktime(good_time))
-  print time.strftime("%m-%d-%Y-%H-%M-%S",good_time)
-
-
-  new_name = podcast_name + '_' + date + "_" + `hour` + ":" + `minute` + ":" + `second`
-  
-  file_in = open(tmpath,"rb")
+  good_time = time.strptime(date + ":" + hour + ":" + minute + ":" + second, "%m:%d%Y:%H:%M:%S")
 
   size = os.stat(tmpath).st_size
   extension = os.path.splitext(tmpath)[1]
 
-  print size
-  print extension
+  new_name = podcast_shortname + '_' + date + "_" + good_time + extension
+
+  print tmpath
+  audio = mutagen.File(tmpath, easy=True)
+  audio["title"] = podcast_name
+  audio["date"] = year + "-" + month + "-" + day
+  audio["artist"] = podcast_station
+  audio.pprint()
+  audio.save()
+  # now rename the file
+
+  file_in = open(tmpath,"rb")
+  file_out = open(storage + "/" + new_name, "wb")
+
+  file_out.write(file_in.read())
+
+  file_in.close()
+  file_out.close()
+  test = mutagen.File(storage + "/" + new_name, easy=True)
+  print test.pprint()
 
 
+# available tags: ['albumartistsort', 'musicbrainz_albumstatus', 'lyricist', 'releasecountry', 'date', 'performer', 'musicbrainz_albumartistid', 'composer', 'encodedby', 'tracknumber', 'musicbrainz_albumid', 'album', 'asin', 'musicbrainz_artistid', 'mood', 'copyright', 'author', 'media', 'length', 'version', 'artistsort', 'titlesort', 'discsubtitle', 'website', 'musicip_fingerprint', 'conductor', 'compilation', 'barcode', 'performer:*', 'composersort', 'musicbrainz_discid', 'musicbrainz_albumtype', 'genre', 'isrc', 'discnumber', 'musicbrainz_trmid', 'replaygain_*_gain', 'musicip_puid', 'artist', 'title', 'bpm', 'musicbrainz_trackid', 'arranger', 'albumsort', 'replaygain_*_peak', 'organization']
