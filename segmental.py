ca8a6cb4f9f9a96a514e2678c4ca65dae0ef6c37
Kennric <kennric@hypothetical.net>
2011-08-18 16:47:15 -0700

    sort each days segments by timestamp

--- segmental.py
+++ segmental.py
@@ -1,3 +1,11 @@
+class AutoVivification(dict):
+    """Implementation of perl's autovivification feature."""
+    def __getitem__(self, item):
+        try:
+            return dict.__getitem__(self, item)
+        except KeyError:
+            value = self[item] = type(self)()
+            return value
 import rfc822
 import os
 import ConfigParser
@@ -5,6 +13,7 @@ from datetime import datetime, date, time
 import re
 import time
 import mutagen
+from operator import itemgetter
 
 # Create an RFC822 compliant date (current time)
 today = date.today()
@@ -21,18 +30,30 @@ full = config.get('options','full')
 url = config.get('files','url')
 
 files = os.listdir(tmpdir)
+segments = AutoVivification()
+#segments = dict()
 
 for file in files:
-  fileparts = file.split('_')
+  file_parts = os.path.splitext(file)
+  basename = file_parts[0]
+  ext = file_parts[1]
 
-  file_dict[file] = ("extension"
-  if segments == True:
-    
+# filename: ktp_2009-03-04_12:03:13.mp3
+  fileparts = basename.split('_')
+  dt = fileparts[1]
+  tm = fileparts[2]
+  datetime = time.strptime(dt + "_" + tm, "%Y-%m-%d_%H:%M:%S")
+  timestamp = time.mktime(datetime)
 
-  if full == '1':
-    outfile = storage + '/' + new_name + extension
-    file_in.seek(0)
-    full_out = open(outfile, 'a')
-    full_out.write(file_in.read())
-    full_out.close()
+  segments[dt][timestamp] = basename
+
+p = iter(sorted(segments.iteritems()))
+
+for k,v in p:
+  print k
+  #print v
+  g = sorted(v,key=lambda x: (v[x]))
+  for timestamp in g:
+    print segments[k][timestamp]
+
 
