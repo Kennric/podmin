2865c8816239f23886573a1ab13bfef597aaf18c
Kennric <kennric@hypothetical.net>
2011-08-25 12:09:58 -0700

    editing template file

--- settings.py
+++ settings.py
@@ -107,8 +107,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    # '/home/kennric/projects/podcaster/templates',
-    os.path.join(os.path.basename(__file__), 'templates'),
+    '/home/kennric/projects/podcaster/templates',
+    #os.path.join(os.path.basename(__file__), 'templates'),
 )
 
 INSTALLED_APPS = (
