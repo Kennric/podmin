8ea7e362b0f3fdf4e09f238a02e50d1310bc3650
Kennric <kennric@hypothetical.net>
2013-06-26 18:37:40 -0700

    import podmin urls

--- urls.py
+++ urls.py
@@ -1,17 +1,18 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'', include('podmin.urls')),
     # Examples:
-    # url(r'^$', 'pc.views.home', name='home'),
     # url(r'^pc/', include('pc.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
 )
