cd570bd22c3bc4c0ec3ced3efa94d5f4a5db2d19
Kennric <kennric@hypothetical.net>
2013-06-28 13:33:14 -0700

    remove fields from forms, add file upload to episode

--- podmin/forms.py
+++ podmin/forms.py
@@ -1,10 +1,18 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, FileField, BooleanField
 from podmin.models import Episode, Podcast
 
+
 class EpisodeForm(ModelForm):
+    upload_file = FileField()
+    tag_audio = BooleanField()
+
     class Meta:
         model = Episode
+        exclude = ('podcast', 'filename', 'pub_date', 'size', 'length')
+
 
 class PodcastForm(ModelForm):
     class Meta:
         model = Podcast
+        exclude = ('updated', 'last_import', 'combine_segments',
+                   'publish_segments', 'tmp_dir', 'up_dir', 'cleaner')
