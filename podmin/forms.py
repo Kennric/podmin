03c87809041719f93c28a4a9b8880b59124b1386
Kennric <kennric@hypothetical.net>
2013-09-21 11:22:42 -0700

    add pubdate and other small fixes to form

--- podmin/forms.py
+++ podmin/forms.py
@@ -1,18 +1,22 @@
-from django.forms import ModelForm, FileField, BooleanField
+from django.forms import ModelForm, FileField, BooleanField, DateTimeField
+from django.forms.widgets import SplitDateTimeWidget
 from podmin.models import Episode, Podcast
-
+import datetime
 
 class EpisodeForm(ModelForm):
     upload_file = FileField()
-    tag_audio = BooleanField()
+    tag_audio = BooleanField(required=False)
+    rename_file = BooleanField(required=False)
+    pub_date = DateTimeField(initial=datetime.date.today)
 
     class Meta:
         model = Episode
-        exclude = ('podcast', 'filename', 'pub_date', 'size', 'length')
+        exclude = ('podcast', 'filename', 'size', 'length')
+        widgets = {'pub_date': SplitDateTimeWidget()}
 
 
 class PodcastForm(ModelForm):
     class Meta:
         model = Podcast
         exclude = ('updated', 'last_import', 'combine_segments',
-                   'publish_segments', 'tmp_dir', 'up_dir', 'cleaner')
+                   'publish_segments', 'up_dir', 'cleaner')
