835695bb6805b67da3a00b57883fd5a85d103d6b
Kennric <kennric@hypothetical.net>
2013-09-22 22:43:12 -0700

    change current to ative in form

--- podmin/forms.py
+++ podmin/forms.py
@@ -1,4 +1,5 @@
-from django.forms import ModelForm, FileField, BooleanField, DateTimeField, CharField
+from django.forms import (ModelForm, FileField, BooleanField, DateTimeField,
+                          CharField)
 from django.forms.widgets import SplitDateTimeWidget
 from podmin.models import Episode, Podcast
 import datetime
@@ -14,7 +15,7 @@ class EpisodeForm(ModelForm):
     class Meta:
         model = Episode
         fields = ['title', 'subtitle', 'description', 'pub_date', 'tags', 
-                  'guid', 'part', 'current', 'filename', 'upload_file',
+                  'guid', 'part', 'active', 'filename', 'upload_file',
                   'rename_file', 'tag_audio', 'show_notes']
         exclude = ('podcast', 'size', 'length')
 
