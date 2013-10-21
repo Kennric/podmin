c0e4f292b33e6a4b1f1d5356c3ff0585d7960c3a
Kennric <kennric@hypothetical.net>
2013-10-20 20:37:43 -0700

    add feeds urls file

---
+++ podmin/urls_feeds.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, url
+from podmin.feeds import RssPodcastFeed, AtomPodcastFeed
+
+
+urlpatterns = patterns(
+    "",
+
+    # Episode list feed by show (RSS 2.0 and iTunes)
+    url(r"^(?P<podcast_slug>[-\w]+)/rss/$",
+        RssPodcastFeed(), name="podcasts_podcast_feed_rss"),
+    # Episode list feed by show (Atom)
+    url(r"^(?P<podcast_slug>[-\w]+)/atom/$",
+        AtomPodcastFeed(), name="podcasts_podcast_feed_atom"),
+    # Episode list feed by show (Media RSS)
+    # TODO upon request
+)
