65915ceaa67e70291f74683438d85332244aedcf
Kennric <kennric@hypothetical.net>
2014-04-06 13:06:41 -0700

    clean up urls with views pattern

--- podmin/urls.py
+++ podmin/urls.py
@@ -6,24 +6,29 @@ import views
 urlpatterns = patterns('',
     url(r'^login/$', 'django.contrib.auth.views.login',
         {'template_name': 'podmin/login.html'}),
-    url(r'^$', 'podmin.views.index', name='index'),
-    url(r'^podcasts/$', 'podmin.views.podcasts'),
-    url(r'^podcast/(?P<pid>\d+)/$', views.podcast, name='podcast'),
-    url(r'^podcast(?:/edit/(?P<pid>\d+))?/$',
-        views.edit_podcast,
-        name='edit_podcast'),
-    url(r'^podcast/new/$', views.new_podcast, name='new_podcast'),
-    url(r'^episode/(?P<eid>\d+)/$', views.episode, name='episode'),
+)
+
+urlpatterns += patterns('podmin.views',    
+    url(r'^$', 'index', name='index'),
+    url(r'^podcasts/$', 'podcasts'),
+    url(r'^podcast/(?P<pid>\d+)/$', 'podcast', name='podcast'),
+    url(r'^podcast(?:/edit/(?P<pid>\d+))?/$','edit_podcast', name='edit_podcast'),
+    url(r'^podcast/new/$', 'new_podcast', name='new_podcast'),
+    url(r'^episode/(?P<eid>\d+)/$', 'episode', name='episode'),
     url(r'^episode/edit/(?P<eid>\d+)/$',
-        views.edit_episode,
+        'edit_episode',
         name='edit_episode'),
     url(r'^episode/new/(?P<pid>\d+)/$',
-        views.new_episode,
+        'new_episode',
         name='new_episode'),
+    
     #(?:/(?P<title>[a-zA-Z]+))?/$
-    #url(r'^my/$', 'podmin.views.control'),
-    #url(r'^podcast/(\d{1,8})/$', 'podmin.views.edit_podcast'),
-    #url(r'^episode/(\d{1,8})/$', 'podmin.views.edit_podcast'),
+    #url(r'^my/$', 'control'),
+    #url(r'^podcast/(\d{1,8})/$', 'edit_podcast'),
+    #url(r'^episode/(\d{1,8})/$', 'edit_podcast'),
+)
+
+urlpatterns += patterns('', 
     url(r'', include('podmin.urls_feeds'))
 )
 # for handling user-uploaded podcast logo in the future
