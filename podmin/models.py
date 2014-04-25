7ecafb3aba21719a4e032e4fa5e7e2032a0cbab4
Kennric <kennric@hypothetical.net>
2014-04-24 23:28:26 -0700

    fix copyright field

--- podmin/models.py
+++ podmin/models.py
@@ -117,9 +117,10 @@ class Podcast(models.Model):
     author = models.CharField(max_length=255, blank=True, null=True)
     contact = models.EmailField(max_length=255, blank=True, null=True)
     updated = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField('cover art', upload_to=get_image_upload_folder)
+    image = models.ImageField('cover art', upload_to=get_image_upload_folder, 
+        default='settings.STATIC/podcast/default/img/default_cover.png')
     copyright = models.ChoicesField('copyright statement', blank=True, null=True,
-                                 choices=COPYRIGHT_CHOICES)
+        choices=COPYRIGHT_CHOICES)
     copyright_url = models.TextField('copyright url', blank=True, null=True)
     language = models.CharField(max_length=8)
     feedburner_url = models.URLField('FeedBurner URL', blank=True)
