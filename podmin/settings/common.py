f6041208750d658b2526ec807db70568b079f7a9
Kennric <kennric@hypothetical.net>
2014-04-26 19:48:01 -0700

    add logfile and better logging config

--- podmin/settings/common.py
+++ podmin/settings/common.py
@@ -66,32 +66,68 @@ INSTALLED_APPS = (
     'podmin'
 )
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
+
 LOGGING = {
     'version': 1,
-    'disable_existing_loggers': False,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
     'filters': {
-         'require_debug_false': {
-             '()': 'django.utils.log.RequireDebugFalse'
-         }
-     },
+     'require_debug_false': {
+         '()': 'django.utils.log.RequireDebugFalse',
+     }
+    },
+    'filters': {
+     'require_debug_true': {
+         '()': 'django.utils.log.RequireDebugTrue',
+     }
+    },
     'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'django.utils.log.NullHandler',
+        },
+        'console':{
+            'filters': ['require_debug_true'],
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
         'mail_admins': {
             'filters': ['require_debug_false'],
             'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
+            'class': 'django.utils.log.AdminEmailHandler',
+        }        
+        'logfile': {
+            'filters': ['require_debug_true'],
+            'level':'DEBUG',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename': SITE_ROOT + "/logfile",
+            'maxBytes': 50000,
+            'backupCount': 2,
+            'formatter': 'simple',
+        },
     },
     'loggers': {
+        'django': {
+            'handlers': ['null'],
+            'propagate': True,
+            'level': 'INFO',
+        },
         'django.request': {
             'handlers': ['mail_admins'],
             'level': 'ERROR',
-            'propagate': True,
+            'propagate': False,
         },
+        'podmin': {
+            'handlers': ['console', 'logfile'],
+            'level': 'INFO'
+        }
     }
 }
-
