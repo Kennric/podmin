93a8ec617de9b6b3589a060a2be65c9639638d97
Kennric <kennric@hypothetical.net>
2014-10-29 22:05:35 -0700

    try virtual_slug url scheme

--- podmin/urls.py
+++ podmin/urls.py
@@ -3,6 +3,17 @@ from django.conf.urls import patterns, url, include
 #from django.conf import settings
 #import views
 
+"""
+The web server gives us a slug to load up if we are called as a virtual
+domain. i.e. mysite.com/podcasts should result in the podcast/mysite view
+"""
+
+try:
+    virtual_slug = request.environ['PODMIN_SLUG']
+except:
+    virtual_slug = False
+
+
 urlpatterns = patterns('', url(
     r'^login/$', 'django.contrib.auth.views.login',
     {'template_name': 'podmin/login.html'}),
@@ -10,30 +21,44 @@ urlpatterns = patterns('', url(
 
 urlpatterns += patterns('', url(r'', include('podmin.urls_feeds')))
 
-urlpatterns += patterns(
-    'podmin.views',
-    url(r'^$', 'index', name='index'),
-    url(r'^(?P<subsite>[A-Za-z0-9/]*)podcasts/$',
-        'podcasts', name='podcasts_list'),
-    url(r'^(?P<subsite>[A-Za-z0-9/]*)podcast/(?P<slug>[A-Za-z0-9\-]+)/$',
-        'podcast', name='podcast_show'),
-    url(r'^(?P<subsite>[A-Za-z0-9/]*)podcast/edit/(?P<slug>[A-Za-z0-9\-]+)/$',
-        'edit_podcast', name='podcast_edit'),
-    url(r'^(?P<subsite>[A-Za-z0-9/]*)podcast/new/$',
-        'new_podcast', name='podcast_new'),
-    url(r'^(?P<subsite>[A-Za-z0-9/]*)episode/(?P<eid>\d+)/$',
-        'episode', name='episode_show'),
-    url(r'^(?P<subsite>[A-Za-z0-9/]*)episode/edit/(?P<eid>\d+)/$',
-        'edit_episode', name='episode_edit'),
-    url(r'^(?P<subsite>[A-Za-z0-9/]*)episode/new/(?P<pid>\d+)/$',
-        'new_episode', name='episode_new'),
-    url(r'^(?P<subsite>\w+)/$', 'index', name='index'),
+if virtual_slug:
+    urlpatterns += patterns(
+        'podmin.views',
+        url(r'^(?P<subsite>[A-Za-z0-9/]*)/$',
+            'podcast', name='podcast_show', {'slug': virtual_slug}),
 
-    #(?:/(?P<title>[a-zA-Z]+))?/$
-    #url(r'^my/$', 'control'),
-    #url(r'^podcast/(\d{1,8})/$', 'edit_podcast'),
-    #url(r'^episode/(\d{1,8})/$', 'edit_podcast'),
-)
+        url(r'^(?P<subsite>[A-Za-z0-9/]*)edit/$',
+            'edit_podcast', name='podcast_edit', {'slug': virtual_slug}),
+        url(r'^(?P<subsite>[A-Za-z0-9/]*)episode/(?P<eid>\d+)/$',
+            'episode', name='episode_show'),
+        url(r'^(?P<subsite>[A-Za-z0-9/]*)episode/edit/(?P<eid>\d+)/$',
+            'edit_episode', name='episode_edit'),
+        url(r'^(?P<subsite>[A-Za-z0-9/]*)episode/new/(?P<pid>\d+)/$',
+            'new_episode', name='episode_new'), {'slug': virtual_slug},
+        url(r'^(?P<subsite>\w+)/$', 'index', name='index'),
+    )
+
+else:
+    urlpatterns += patterns(
+        'podmin.views',
+        url(r'^$', 'index', name='index'),
+        url(r'^podcasts/$',
+            'podcasts', name='podcasts_list'),
+        url(r'^podcast/(?P<slug>[A-Za-z0-9\-]+)/$',
+            'podcast', name='podcast_show'),
+        url(r'^podcast/edit/(?P<slug>[A-Za-z0-9\-]+)/$',
+            'edit_podcast', name='podcast_edit'),
+        url(r'^podcast/new/$',
+            'new_podcast', name='podcast_new'),
+        url(r'^episode/(?P<eid>\d+)/$',
+            'episode', name='episode_show'),
+        url(r'^episode/edit/(?P<eid>\d+)/$',
+            'edit_episode', name='episode_edit'),
+        url(r'^episode/new/(?P<pid>\d+)/$',
+            'new_episode', name='episode_new'),
+
+        #(?:/(?P<title>[a-zA-Z]+))?/$
+    )
 
 
 # for handling user-uploaded podcast logo in the future
