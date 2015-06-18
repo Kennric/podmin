7c4c37462d00fac1787700d17d527385d00d3d9f
Kennric <kennric@hypothetical.net>
2015-06-18 10:35:53 -0700

    update, cleanup settings files

--- podmin/settings/dev.py
+++ podmin/settings/dev.py
@@ -11,26 +11,9 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
-
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
-
-BACKLOG_ROOT = '/media/backlog'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
@@ -54,9 +37,29 @@ Make sure you set:
     PODMIN_DEV_DB_HOST
     PODMIN_DEV_DB_PORT
     PODMIN_DEV_SECRET_KEY
+    PODMIN_DEV_MEDIA_ROOT
+    PODMIN_DEV_MEDIA_URL
+    PODMIN_DEV_STATIC_ROOT
+    PODMIN_DEV_BUFFER_ROOT
 from somewhere before the settings are loaded.
 """
 
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = os.environ['PODMIN_STG_MEDIA_ROOT']
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = os.environ['PODMIN_STG_MEDIA_URL']
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = os.environ['PODMIN_STG_STATIC_ROOT']
+
+BUFFER_ROOT = os.environ['PODMIN_STG_BUFFER_ROOT']
 
 DATABASES = {
     'default': {
