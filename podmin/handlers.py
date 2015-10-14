970c3a40ae9f31fc69b6c59377e1b367978e05af
Kennric <kennric@hypothetical.net>
2015-10-13 23:09:47 -0700

    fix image paths

--- podmin/handlers.py
+++ podmin/handlers.py
@@ -79,10 +79,10 @@ def podcast_post_delete(sender, **kwargs):
 @receiver(post_delete, sender=Episode)
 def episode_post_delete(sender, **kwargs):
     media_path = path.join(settings.MEDIA_ROOT,
-        kwargs['instance'].podcast.slug)
+        kwargs['instance'].podcast.slug, "img")
 
     buffer_path = path.join(settings.BUFFER_ROOT,
-        kwargs['instance'].podcast.slug)
+        kwargs['instance'].podcast.slug, "img")
 
     image_name, ext = path.splitext(path.basename(
         kwargs['instance'].buffer_image.name))
