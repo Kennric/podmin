f0be70171aa2d2ec8bb1a7fdfade19fb5d16b650
Kennric <kennric@hypothetical.net>
2015-12-21 20:21:43 -0800

    migration for realnames

---
+++ podmin/migrations/0006_auto_20151221_2020.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('podmin', '0005_remove_podcast_mothball_dir'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='podcast',
+            name='editor_name',
+            field=models.CharField(max_length=255, null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='podcast',
+            name='webmaster_name',
+            field=models.CharField(max_length=255, null=True, blank=True),
+        ),
+    ]
